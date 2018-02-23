2c0b7991b3857a911197ed7145e0f519d1e8280a
omar <b00056993@aus.edu>
2018-02-23 21:58:50 +0400

    added feature B

--- src/lab3/main.java
+++ src/lab3/main.java
@@ -2,6 +2,9 @@ package lab3;
 
 public class main {
 
+	public void featureB(){ 
+	System.out.println("featureB");
+	}
 	public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
