4768c28cf8af6b0b5f15c26cef224cb088bbe777
omar <b00056993@aus.edu>
2018-02-23 21:56:00 +0400

    first commit

---
+++ src/lab3/main.java
@@ -0,0 +1,9 @@
+package lab3;
+
+public class main {
+
+	public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
